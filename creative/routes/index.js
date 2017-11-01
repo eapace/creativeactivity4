517b12ab097a50b28609d7f6489189da972c1683
eapace <paceadriana1@gmail.com>
2017-11-01 22:19:41 +0000

    lab 4 copy

--- creative/routes/index.js
+++ creative/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var https = require('https')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('weather.html', { root: 'public' });
+});
+
+router.get('/owlapi', function(req, res1, next) {
+  const https = require('https');
+  var result = '';
+
+  https.get('https://owlbot.info/api/v1/dictionary/' + req.query.q, (res) => {
+   // console.log('statusCode:', res.statusCode);
+   // console.log('headers:', res.headers);
+
+    var jsonresult = [];
+    res.on('data', (d) => {
+      process.stdout.write(d);
+      result += d;
+    })
+
+  .on('end', (e) => {
+    console.log(e);
+   res1.status(200).json(JSON.parse(result));
+   });
+  });
+ // console.log(JSON.parse(result));
 });
 
 module.exports = router;
+
